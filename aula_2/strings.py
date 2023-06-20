a557fb69cbd434652c6ec2781d8fb3ec528b0fdc
Wesley Ribeiro <weslenribeiro@hotmail.com>
2023-06-19 22:06:50 -0300

    O arquivo de strings

---
+++ aula_2/strings.py
@@ -0,0 +1,26 @@
+s = "banana";
+s = s.replace("b", "z");
+
+print(s, id(s));
+
+fruta = 'banana';
+
+print(fruta);
+
+
+# Exemplo de programa que aborda o tipo 
+
+nome = "Wesley"
+sobrenome = "Ribeiro"
+
+print(nome + sobrenome);
+
+
+frase = "Olá, Mundo!";
+print("String original: ");
+print(frase);
+print();
+
+print("Acessando caracteres individuais: ");
+print("Primeiro caractere: ", frase[0]);
+print("Ultimo caractere ", frase[-1]);
