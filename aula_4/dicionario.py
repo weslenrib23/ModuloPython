5b2fc3976de7f7fd422efa10de14a1292538f91c
Wesley Ribeiro <weslenribeiro@hotmail.com>
2023-06-28 20:46:08 -0300

    Atualizando aula chave e valor

--- aula_4/dicionario.py
+++ aula_4/dicionario.py
@@ -5,3 +5,45 @@ meuDicionario = {
     };
 
 print(meuDicionario);
+
+frutaDicionario = { 
+    'maçã'   : 3,
+    'banana' : 6,
+    'uva'    : 8,
+};
+
+print("Significado encontrado no dicionário : ", frutaDicionario["maçã"]);
+print();
+
+frutaDicionario["maçã"] = 5;
+frutaDicionario["laranja"] = 10;
+
+print("Nova quantidade de maçã: ", frutaDicionario["maçã"]);
+print();
+print(frutaDicionario);
+
+print(frutaDicionario);
+print();
+
+animaisDicionario = {};
+animaisDicionario["cachorro"] = "Melissa";
+print(animaisDicionario);
+print();
+
+aluno = {
+    'nome'     : 'Wesley', 
+    'anos'     : 37 ,
+    'hobbies'  : ['jogar', 'esportes']
+    };
+
+print(aluno);
+print();
+
+frutaDicionario = { 
+    'maçã'   : 3,
+    'banana' : 6,
+    'uva'    : 8,
+    'vitaminas' : {'a' : 'boa pra ele'}
+};
+
+print(frutaDicionario["vitaminas"]["a"]);
