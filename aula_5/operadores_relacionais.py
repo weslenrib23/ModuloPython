d09f208d56d3dfb8053fe4c46fdd69840d861b72
Wesley Ribeiro <weslenribeiro@hotmail.com>
2023-06-29 20:24:20 -0300

    Operadores Relacionais

---
+++ aula_5/operadores_relacionais.py
@@ -0,0 +1,31 @@
+# Operadores Relacionais
+
+# -------------------------------
+
+x = 5;
+y = 7;
+
+
+# Maior que
+print(x > y); #False
+print();
+
+# Menor que
+print(x < 7); #True
+print();
+
+# Igualdade
+print(x == y); #False
+print();
+
+# Maior ou igual a
+print(x >= y); #False
+print();
+
+# Menor ou igual a
+print(x <= y); #True
+print();
+
+# Diferente de
+print(x != y); #True
+print();
