513b4cea32e135be5bbc120aca09baff24beb6e0
Wesley <weslenribeiro@hotmail.com>
2023-07-13 22:10:17 -0300

    Atualizado do bancaria

--- POO/conta_bancaria.py
+++ POO/conta_bancaria.py
@@ -7,19 +7,27 @@ class Conta:
     def depositar(self, valor):
         self.saldo += valor;
 
-    def depositar(self, valor):
+    def sacar(self, valor):
         if valor <= self.saldo:
             self.saldo -= valor;
         else:
-           print("Saldo insulficiente!");
+           print("Saldo insuficiente!");
 
     def exibir_informacoes(self):
         print(f"Conta: {self.numero}");
         print(f"Titular: {self.titular}"); 
-        print(f"Saldo: {self.saldo}");
+        # print(f"Saldo: {self.saldo:,.2f}");
+        valorReal = f'R${self.saldo:_.2f}';
+        valorReal = valorReal.replace('.',',').replace('_','.');
+
+        print(f"Saldo: {valorReal}")
 
 conta = Conta(123, "Wesley");
-
 conta.depositar(1000);
 conta.sacar(500);
 conta.exibir_informacoes();
+
+contaZoio = Conta(321, "Zoio");
+contaZoio.depositar(1500);
+contaZoio.sacar(350);
+contaZoio.exibir_informacoes();
